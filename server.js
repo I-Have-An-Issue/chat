32bbcd217648c059faabf5fa1616741137772662
I-Have-An-Issue <34550332+I-Have-An-Issue@users.noreply.github.com>
2022-10-12 17:20:59 -0400

    Wowie

--- server.js
+++ server.js
@@ -10,15 +10,93 @@ app.use(handler)
 
 const wss = new WebSocketServer({ server })
 
-wss.on("connection", (ws) => {
+let users = {}
+let past_messages = []
+
+function broadcast(data, ws) {
+	past_messages.push(JSON.stringify(data))
+	wss.clients.forEach((client) => {
+		if (client !== ws && client.readyState === WebSocket.OPEN) {
+			client.send(JSON.stringify(data))
+		}
+	})
+}
+
+wss.on("connection", (ws, request) => {
+	for (let i = 0; i < past_messages.length; i++) {
+		let pastMessage = past_messages[i]
+		ws.send(pastMessage)
+	}
+
+	users[`${request.socket.remoteAddress}:${request.socket.remotePort}`] = {}
+	ws.send(JSON.stringify({ type: "SERVER_MESSAGE", content: "The server has enabled guest login. Your next message will be your username." }))
+	users[`${request.socket.remoteAddress}:${request.socket.remotePort}`].state = "SET_USERNAME"
+
+	ws.on("pong", () => (ws.isAlive = true))
+
 	ws.on("message", (message, isBinary) => {
-		const data = JSON.parse(message.toString())
-		wss.clients.forEach(function each(client) {
-			if (client !== ws && client.readyState === WebSocket.OPEN) {
-				client.send(message, { binary: isBinary })
+		let data
+		try {
+			data = JSON.parse(message.toString())
+		} catch (_) {}
+
+		if (users[`${request.socket.remoteAddress}:${request.socket.remotePort}`]) {
+			switch (users[`${request.socket.remoteAddress}:${request.socket.remotePort}`].state) {
+				case "SET_USERNAME":
+					if (!data.content || data.content.length <= 0) return ws.send(JSON.stringify({ type: "SERVER_MESSAGE", content: "You must enter a username." }))
+					if (data.content.length > 16) return ws.send(JSON.stringify({ type: "SERVER_MESSAGE", content: "Usernames can only be 1-16 characters long." }))
+					if (new RegExp(/[^A-Za-z0-9_]/g).test(data.content)) return ws.send(JSON.stringify({ type: "SERVER_MESSAGE", content: "Usernames can only be letters, numbers, and underscores." }))
+					if (Object.values(users).find((_) => _.username == data.content.toLowerCase())) return ws.send(JSON.stringify({ type: "SERVER_MESSAGE", content: "Somebody already has that username." }))
+
+					users[`${request.socket.remoteAddress}:${request.socket.remotePort}`].state = "READY"
+					users[`${request.socket.remoteAddress}:${request.socket.remotePort}`].username = data.content
+					return broadcast({ type: "SERVER_MESSAGE", content: `${data.content} has joined the chatroom.`, timestamp: Date.now() })
+					break
+				case "READY":
+					if (data.content.length > 100 || data.content.length <= 0) return ws.send(JSON.stringify({ type: "SERVER_MESSAGE", content: "Messages can only be 1-100 characters long." }))
+
+					setTimeout(() => (users[`${request.socket.remoteAddress}:${request.socket.remotePort}`].state = "READY"), 500)
+					users[`${request.socket.remoteAddress}:${request.socket.remotePort}`].state = "COOLDOWN"
+
+					return broadcast({
+						type: "USER_MESSAGE",
+						author: users[`${request.socket.remoteAddress}:${request.socket.remotePort}`].username,
+						content: data.content,
+						timestamp: Date.now(),
+					})
+					break
+				case "COOLDOWN":
+					break
+				default:
+					return ws.send(JSON.stringify({ type: "SERVER_MESSAGE", content: "ERR_UNKNOWN_STATE" }))
+					break
 			}
-		})
+		}
+	})
+
+	ws.on("close", (code, reason) => {
+		if (users[`${request.socket.remoteAddress}:${request.socket.remotePort}`] && users[`${request.socket.remoteAddress}:${request.socket.remotePort}`].username) {
+			broadcast({
+				type: "SERVER_MESSAGE",
+				content: `${users[`${request.socket.remoteAddress}:${request.socket.remotePort}`].username} has left the chatroom.`,
+				timestamp: Date.now(),
+			})
+
+			delete users[`${request.socket.remoteAddress}:${request.socket.remotePort}`]
+		}
 	})
 })
 
-server.listen(process.env.PORT)
+const interval = setInterval(() => {
+	wss.clients.forEach((ws) => {
+		if (ws.isAlive === false) return ws.terminate()
+
+		ws.isAlive = false
+		ws.ping()
+	})
+}, 30000)
+
+wss.on("close", () => clearInterval(interval))
+
+server.listen(process.env.PORT || 5173)
+console.log(`Listening on http://127.0.0.1:${process.env.PORT || 5173}/`)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,24 @@
 <script>
 	import { browser } from "$app/environment"
 	import { page } from "$app/stores"
-	import { onMount } from "svelte"
 
 	export let chats = []
 	let valid = true,
 		connected = false,
-		username,
 		value = "",
 		chatbox,
-		socket
+		socket,
+		socketUrl = `ws${$page.url.protocol == "https:" ? "s" : ""}://${$page.url.host}/`,
+		target
 
 	function onInput() {
-		if (value.length > 50) valid = false
+		if (value.length > 100) valid = false
 		else valid = true
 	}
 
 	function sendMessage() {
-		if (value.length == 0 || !connected) return
-		if (!username && new RegExp(/[^A-Za-z0-9_]/g).test(value)) return newMessage(Date.now(), "Server", "Usernames can only be letters, numbers, and underscores.")
-		if (!username) {
-			username = value
-			socket.send(JSON.stringify({ timestamp: Date.now(), username, content: "has joined the chatroom." }))
-			value = ""
-			return newServerMessage(`Logged in.`)
-		}
-
-		socket.send(JSON.stringify({ timestamp: Date.now(), username, content: value }))
-		newMessage(Date.now(), username, value)
+		if (value.length == 0 || !connected || !valid) return
+		socket.send(JSON.stringify({ content: value }))
 		value = ""
 	}
 
@@ -38,52 +29,61 @@
 	function newMessage(ms, username, content) {
 		chats.push({ timestamp: ms, username, content })
 		chats = chats
-		chatbox.scrollTop = chatbox.scrollHeight
+		target.scrollIntoView()
 	}
 
 	if (browser) {
-		console.log($page.url, `ws${$page.url.protocol == "https:" ? "s" : ""}://${$page.url.host}/`)
-		socket = new WebSocket(`ws${$page.url.protocol == "https:" ? "s" : ""}://${$page.url.host}/`)
+		socket = new WebSocket(socketUrl)
 
-		socket.addEventListener("open", () => {
-			connected = true
-			newServerMessage("Connected.")
-			newServerMessage("Set a username to enter the chatroom.")
-		})
+		socket.addEventListener("open", () => (connected = true))
+		socket.addEventListener("error", (error) => console.log(error))
 
 		socket.addEventListener("close", () => {
 			connected = false
 			newServerMessage("You have lost connection to the chatroom.")
 		})
 
-		socket.addEventListener("error", (error) => {
-			console.log(error)
-		})
-
 		socket.addEventListener("message", ({ data }) => {
-			if (!username) return
 			const json = JSON.parse(data.toString())
-			newMessage(new Date(json.timestamp).getTime(), json.username, json.content)
+
+			switch (json.type) {
+				case "SERVER_MESSAGE":
+					newServerMessage(json.content)
+					break
+				case "USER_MESSAGE":
+					newMessage(new Date(json.timestamp).getTime(), json.author, json.content)
+					break
+				default:
+					console.log(json)
+					break
+			}
 		})
 	}
-
-	onMount(() => {
-		newServerMessage("Connecting...")
-	})
 </script>
 
-<div class="mx-auto max-w-lg p-4 border-2 border-gray-200 rounded-lg divide-y">
-	<div class="flex flex-col h-[30rem] overflow-auto pb-1" bind:this={chatbox}>
+<div class="mx-auto max-w-4xl p-4 border-2 border-gray-200 rounded-lg divide-y">
+	<div class="flex flex-col h-[30rem] overflow-auto" bind:this={chatbox}>
 		{#each chats as message}
 			<div class="flex flex-row">
 				<p class="text-black text-opacity-50 mr-2 whitespace-nowrap">{new Date(message.timestamp).toLocaleTimeString()}</p>
 				<p class="font-bold mr-2 whitespace-nowrap">{message.username}</p>
-				<p class="">{message.content}</p>
+				<p>{message.content}</p>
+			</div>
+		{:else}
+			<div class="flex flex-row">
+				<p class="text-black text-opacity-50 mr-2 whitespace-nowrap">{new Date().toLocaleTimeString()}</p>
+				<p class="font-bold mr-2 whitespace-nowrap">{"[Server]"}</p>
+				<p>Connecting to the chatroom...</p>
 			</div>
 		{/each}
+		<div class="flex flex-row select-none text-white" bind:this={target}>
+			<p class="mr-2 whitespace-nowrap">{new Date().toLocaleTimeString()}</p>
+			<p class="font-bold mr-2 whitespace-nowrap">{"[Server]"}</p>
+			<p>Filler text</p>
+		</div>
 	</div>
-	<form on:submit|preventDefault={sendMessage} class="pt-3 flex">
-		<input class="h-10 w-full px-2 rounded border-2 border-grey-300" type="text" id="message" bind:value on:input={onInput} />
-		<button class="px-2 rounded ml-2 border-2 border-blue-500 {valid ? 'text-blue-500 hover:bg-blue-500 hover:bg-opacity-30' : 'text-blue-100 border-blue-100 pointer-events-none'}">Send</button>
+	<form on:submit|preventDefault={sendMessage} class="flex pt-3">
+		<input class="h-10 w-full px-2 rounded border-2 border-grey-300" type="text" id="message" autocomplete="off" bind:value on:input={onInput} />
+		<button class="px-2 rounded ml-2 border-2 border-blue-500 {valid || connected ? 'text-blue-500 hover:bg-blue-500 hover:bg-opacity-30' : 'text-blue-100 border-blue-100 pointer-events-none'}">Send</button>
 	</form>
 </div>
